9180ed49dd2ee46c28312df9cbdc2a53e7660223
dada007007 <56538171+dada007007@users.noreply.github.com>
2023-11-12 00:47:02 +0800

    Add files via upload

---
+++ leetcode/src/easy/e69_mySqrt/Test.java
@@ -0,0 +1,9 @@
+package easy.e69_mySqrt;
+
+public class Test {
+    public static void main(String[] args) {
+        MySqrt mySqrt = new MySqrt();
+        int x = mySqrt.mySqrt(8);
+        System.out.println(x);
+    }
+}
---
+++ leetcode/src/easy/e70_climbStairs/Test.java
@@ -0,0 +1,9 @@
+package easy.e70_climbStairs;
+
+public class Test {
+    public static void main(String[] args) {
+        ClimbStairs cs = new ClimbStairs();
+        System.out.println(cs.climbStairs(2));
+
+    }
+}
